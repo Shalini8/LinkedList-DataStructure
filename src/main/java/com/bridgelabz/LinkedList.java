57e340c696721799bc9271aeda3d42aaa572dd08
Shalini8 <shalu8mar@gmail.com>
2021-05-01 20:35:05 +0530

    [Modified] apend 30 n 70 to 56 in a linkedlist

--- src/main/java/com/bridgelabz/LinkedList.java
+++ src/main/java/com/bridgelabz/LinkedList.java
@@ -21,6 +21,17 @@ public class LinkedList<K> {
             this.head.setNext(tempNode);
         }
     }
+    public void append(INode newNode){
+        if(this.head == null) {
+            this.head = newNode;
+        }if(this.tail == null) {
+            this.tail = newNode;
+        }else {
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+    }
+
     public void printNodes() {
         StringBuffer nodes = new StringBuffer("My Nodes: ");
         INode tempNode = head;
--- src/test/java/com/bridgelabz/LinkedListTest.java
+++ src/test/java/com/bridgelabz/LinkedListTest.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class NodeTest {
+public class LinkedListTest {
     @Test
 public void Given3Numbers_WhenLinked_ShouldPassed_LinkedListTest() {
     Node<Integer> firstNode = new Node<Integer>(56);
@@ -28,6 +28,21 @@ public void Given3Numbers_WhenLinked_ShouldPassed_LinkedListTest() {
         boolean res = linkedList.head.equals(thirdNode) &&
                 linkedList.head.getNext().equals(secondNode) &&
                 linkedList.tail.equals(firstNode);
+        Assertions.assertTrue(res);
+    }
+    @Test
+    public void Given3Numbers_WhenAppended_ToLinkedListShould_BeAddedToLast() {
+        Node<Integer> firstNode = new Node<Integer>(56);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(70);
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(firstNode);
+        linkedList.append(secondNode);
+        linkedList.append(thirdNode);
+        linkedList.printNodes();
+        boolean res = linkedList.head.equals(firstNode) &&
+                linkedList.head.getNext().equals(secondNode) &&
+                linkedList.tail.equals(thirdNode);
         Assertions.assertTrue(res);
     }
 }
