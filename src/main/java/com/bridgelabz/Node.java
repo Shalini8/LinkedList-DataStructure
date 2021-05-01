76b36be4081ffb4a2aa3d2c09e66ad94a0ecf7e6
Shalini8 <shalu8mar@gmail.com>
2021-05-01 16:02:29 +0530

    [Add] Create a linkedlist

--- src/main/java/com/bridgelabz/Node.java
+++ src/main/java/com/bridgelabz/Node.java
@@ -1,7 +1,29 @@
 package com.bridgelabz;
 
-public class Node {
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO LINKEDLIST ");
+public class Node<K> {
+    public Node getNext() {
+        return next;
     }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    private Node next;
+    private K key;
+
+    public Node(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+
 }
--- src/test/java/com/bridgelabz/NodeTest.java
+++ src/test/java/com/bridgelabz/NodeTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz;
 
-public class NodeTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NodeTest { @Test
+public void Given3Numbers_WhenLinked_ShouldPassed_LinkedListTest() {
+    Node<Integer> firstNode = new Node<Integer>(56);
+    Node<Integer> secondNode = new Node<Integer>(30);
+    Node<Integer> thirdNode = new Node<Integer>(70);
+    firstNode.setNext(secondNode);
+    secondNode.setNext(thirdNode);
+    boolean res = firstNode.getNext().equals(secondNode) &&
+            secondNode.getNext().equals(thirdNode);
+    Assertions.assertTrue(res);
+}
 }
